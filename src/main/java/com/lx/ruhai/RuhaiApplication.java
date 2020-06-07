b174a43aa3be35f6bf3a5f099170197d496358e9
crow <l0serh40615@gamil.com>
2020-06-07 23:16:43 +0800

    init project

---
+++ src/main/java/com/lx/ruhai/RuhaiApplication.java
@@ -0,0 +1,13 @@
+package com.lx.ruhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuhaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuhaiApplication.class, args);
+    }
+
+}
